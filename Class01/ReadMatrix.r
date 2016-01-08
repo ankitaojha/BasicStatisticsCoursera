7251f1c72623db613ed00ef41a7b94d099ccb2df
ankitaojha <ankitaojha6888@gmail.com>
2016-01-09 00:26:35 +0530

    Create ReadMatrix.r

---
+++ Class01/ReadMatrix.r
@@ -0,0 +1,6 @@
+# load in the data and store it in the variable cars
+
+cars <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/course/uva/mtcars.csv")
+# print the first 6 rows of the dataset using the head() function
+
+head(cars,n=6)
